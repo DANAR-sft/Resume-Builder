e3a7e0877af2b4c06e057d462c7f246b3396b162
Yudha <yudhasanggrama@gmail.com>
2026-02-23 11:40:25 +0700

    refactor: server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
 import { app } from "./app";
 
-const PORT = Number(process.env.PORT ?? 5000);
+const PORT = Number(process.env.PUBLIC_BASE_URL ?? 5000);
 app.listen(PORT, () => console.log(`API running: http://localhost:${PORT}`));
